928255541f543230b7a8801070d95d50e473c7e8
YuShinPeng <ccsauce173@gmail.com>
2023-05-10 20:29:55 +0800

    first commit

---
+++ src/main/java/com/example/fruit_grammy_java/FruitGrammyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fruit_grammy_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitGrammyJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitGrammyJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fruit_grammy_java/FruitGrammyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fruit_grammy_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitGrammyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
