4792202234ad27909896001782662d7ae70e9aea
YuShinPeng <ccsauce173@gmail.com>
2023-05-30 20:23:40 +0800

    0530-1 YuShin commit

--- src/main/java/com/example/fruit_grammy_java/impl/ProductServiceImpl.java
+++ src/main/java/com/example/fruit_grammy_java/impl/ProductServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.fruit_grammy_java.impl;
 
-
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -23,8 +22,7 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	private ProductDao productDao;
 
-
-	// 嚙誼查嚙踝蕭嚙踝蕭X嚙糊嚙褊私嚙踝蕭嚙踝蕭k
+	// 檢查日期合法性私有方法
 	private boolean isValidDate(String date) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		dateFormat.setLenient(false);
@@ -37,157 +35,147 @@ public class ProductServiceImpl implements ProductService {
 
 	}
 
-	// 嚙踝蕭a嚙磕嚙稼嚙編嚙諉品
-	// 嚙磕嚙稼嚙豎塚蕭嚙確
-
-	// 鞈�摰嗡������
-	// 銝��憛怨���
-
+	// 賣家上架新商品
+	// 上架需填資訊
 	@Override
 	public ProductResponse addProduct(ProductRequest productReq) {
-//		// 鋆炎�鞈��ist
+		// 裝檢查資料的List
 		List<Product> productList = productReq.getProductList();
-//
+
 		for (Product item : productList) {
 
 			if (!StringUtils.hasText(item.getHsCode())) {
-				return new ProductResponse("���楊蝣潔��蝛�");
+				return new ProductResponse("商品編碼不得為空");
 			}
 
 			if (productDao.existsById(item.getHsCode())) {
-				return new ProductResponse("甇文��歇銝");
+				return new ProductResponse("此商品已上架");
 			}
 
 			if (!StringUtils.hasText(item.getSellerAccount())) {
-				return new ProductResponse("鞈�摰嗅董����蝛�");
+				return new ProductResponse("賣家帳號不得為空");
 			}
 
 			if (!StringUtils.hasText(item.getName())) {
-				return new ProductResponse("����迂銝�蝛�");
+				return new ProductResponse("商品名稱不得為空");
 			}
 
 			if (!StringUtils.hasText(item.getType())) {
-				return new ProductResponse("蝔桅���蝛�");
+				return new ProductResponse("種類不得為空");
 			}
 
 			if (!StringUtils.hasText(item.getPlace())) {
-				return new ProductResponse("��銝�蝛�");
+				return new ProductResponse("產地不得為空");
 			}
 
 			if (item.getNumber() <= 0) {
-				return new ProductResponse("���������0");
+				return new ProductResponse("數量不得小於或等於0");
 			}
 
 			if (!StringUtils.hasText(item.getDescription())) {
-				return new ProductResponse("����酉隤芣���蝛�");
+				return new ProductResponse("商品備註說明不得為空");
 			}
 
-			if (!StringUtils.hasText(item.getDate())) {
-				return new ProductResponse("�������蝛�");
-			}
-
-			// 瑼Ｘ����蝚血�撘�������
 			String date = item.getDate();
+			if (!StringUtils.hasText(date)) {
+				return new ProductResponse("採收日期不得為空");
+			}
+
+			// 檢查日期是否符合格式且具有合法性
 			String regex = "\\d{4}-\\d{2}-\\d{2}";
 			if (!date.matches(regex)) {
-				return new ProductResponse("���撘隤�");
+				return new ProductResponse("日期格式錯誤");
 			}
 
+			// 檢查此日期是否合法性 ex. 4044-04-04 不可能
+			if (!isValidDate(date)) {
+				return new ProductResponse("日期格式錯誤");
+			}
 
-			// 嚙誼查嚙踝蕭嚙踝蕭嚙踝蕭O嚙稻嚙碼嚙糊嚙踝蕭 ex. 4044-04-04 嚙踝蕭嚙箠嚙踝蕭
-			if (!isValidDate(item.getDate())) {
-                return new ProductResponse("嚙踝蕭嚙踝蕭嚙踝蕭X嚙糊");
-            }
-	
-		// 檢查日期是否為未來日期
-			LocalDate currentDate = LocalDate.now();
-			LocalDate parsedLocalDate = LocalDate.parse(date);
-			if (parsedLocalDate.isAfter(currentDate)) {
+			// 未來日期不存在
+			if (isValidDate(date)) {
 				return new ProductResponse("不得為未來日期");
 			}
-			
+
 		}
-		// �憓���鞈�����
+		// 新增成功回傳資訊和成功訊息
 		productDao.saveAll(productList);
-		return new ProductResponse(productList, "�憓���");
+		return new ProductResponse(productList, "新增成功");
 	}
 
-	// 鞈�摰嗡耨�銝����
+	// 賣家修改上架商品
 	@Override
 	public ProductResponse updateProduct(ProductRequest productReq) {
 		Optional<Product> findProduct = productDao.findById(productReq.getHsCode());
 		if (!findProduct.isPresent()) {
-			return new ProductResponse("甇文����");
+			return new ProductResponse("此商品不存在");
 		}
 
-		// 摮���迨������
+		// 存在取得此產品資訊
 		Product getProduct = findProduct.get();
 
-		// 靽格����迂
+		// 修改產品名稱
 		if (StringUtils.hasText(productReq.getName())) {
 			getProduct.setName(productReq.getName());
 			productDao.save(getProduct);
 		}
 
-		// 靽格蝔桅��
+		// 修改種類
 		if (StringUtils.hasText(productReq.getType())) {
 			getProduct.setType(productReq.getType());
 			productDao.save(getProduct);
 		}
 
-		// 靽格��
+		// 修改產地
 		if (StringUtils.hasText(productReq.getPlace())) {
 			getProduct.setPlace(productReq.getPlace());
 			productDao.save(getProduct);
 		}
 
-		// 靽格����
+		// 修改數量
 		if (productReq.getNumber() > 0) {
 			getProduct.setNumber(productReq.getNumber());
 			productDao.save(getProduct);
 		}
 
-		// 靽格����
-		if (StringUtils.hasText(productReq.getDate())) {
-			getProduct.setDate(productReq.getDate());
-			// 瑼Ｘ����蝚血�撘�
-			String date = productReq.getDate();
+		String date = productReq.getDate();
+		// 修改日期
+		if (StringUtils.hasText(date)) {
+			getProduct.setDate(date);
+			// 檢查日期是否符合格式
 			String regex = "\\d{4}-\\d{2}-\\d{2}";
 			if (!date.matches(regex)) {
-				return new ProductResponse("���撘隤�");
+				return new ProductResponse("日期格式錯誤");
 			}
-
 			// 檢查此日期是否合法性 ex. 4044-04-04 不可能
-			if (!isValidDate(productReq.getDate())) {
-				return new ProductResponse("日期不存在");
+			if (!isValidDate(date)) {
+				return new ProductResponse("日期格式錯誤");
 			}
 
-			// 檢查日期是否為未來日期
-			LocalDate currentDate = LocalDate.now();
-			LocalDate parsedLocalDate = LocalDate.parse(date);
-			if (parsedLocalDate.isAfter(currentDate)) {
+			// 未來日期不存在
+			if (isValidDate(date)) {
 				return new ProductResponse("不得為未來日期");
 			}
 
 			productDao.save(getProduct);
 		}
 
-		// 靽格��
+		// 修改價格
 		if (productReq.getPrice() > 0) {
 			getProduct.setPrice(productReq.getPrice());
 			productDao.save(getProduct);
 		}
 
-		// 靽格��膩
+		// 修改敘述
 		if (StringUtils.hasText(productReq.getDescription())) {
 			getProduct.setDescription(productReq.getDescription());
 			productDao.save(getProduct);
 		}
 
-		return new ProductResponse("靽格����", getProduct);
+		return new ProductResponse("修改成功", getProduct);
 	}
 
-	// 鞎瑕振���� ��撅交風 -- �������
+	// 買家搜尋 生產履歷 -- 透過產地搜尋
 	@Override
 	public ProductResponse searchPlaceProduct(String place) {
 
@@ -196,13 +184,12 @@ public class ProductServiceImpl implements ProductService {
 		List<ProductResponse> searchAllRes = new ArrayList<>();
 
 		if (!StringUtils.hasText(place)) {
-			return new ProductResponse("���摰嫣��蝛�");
+			return new ProductResponse("搜尋內容不得為空");
 		}
 
 		for (Product item : searchReq) {
 			ProductResponse searchRes = new ProductResponse();
 			searchRes.setPlace(place);
-			searchRes.setHsCode(item.getHsCode());
 			searchRes.setName(item.getName());
 			searchRes.setDate(item.getDate());
 			searchRes.setType(item.getType());
@@ -216,11 +203,11 @@ public class ProductServiceImpl implements ProductService {
 	}
 
 	@Override
-	public ProductResponse searchSpecificProduct(String name) { // 銝�����
+	public ProductResponse searchSpecificProduct(String name) { // 上架的搜尋
 
 		List<Product> searchAllRes = new ArrayList<>();
 
-		if (!StringUtils.hasText(name)) {// 憒�null嚗�����鞈��
+		if (!StringUtils.hasText(name)) {// 如果為null，直接提供全部資料
 
 			List<Product> allSearchReq = productDao.findAll();
 			return new ProductResponse(allSearchReq, "all info");
@@ -229,13 +216,12 @@ public class ProductServiceImpl implements ProductService {
 		List<Product> searchReq = productDao.findByNameContaining(name);
 
 		for (Product item : searchReq) {
-
 			searchAllRes.add(item);
 		}
 		return new ProductResponse(searchAllRes, "specific info");
 	}
 
-	// 鞈�摰嗅歇銝����
+	// 賣家已上架商品
 	@Override
 	public ProductResponse searchSellerProduct(String sellerAccount) {
 		List<Product> searchReq = productDao.findBySellerAccount(sellerAccount);
@@ -255,181 +241,70 @@ public class ProductServiceImpl implements ProductService {
 			searchAllRes.add(searchRes);
 		}
 
-
-//		return new ProductResponse("��������", searchAllRes);
-		return new ProductResponse(searchAllRes);
+		return new ProductResponse("搜尋結果如下", searchAllRes);
 	}
 
-	// ��撌脖�����
+	// 刪除已上架商品
 	@Override
 	public ProductResponse removeProduct(ProductRequest productReq) {
 		Optional<Product> findProduct = productDao.findById(productReq.getHsCode());
 		if (!findProduct.isPresent()) {
-			return new ProductResponse("甇文����");
+			return new ProductResponse("此商品不存在");
 		}
 
 		try {
 			Product product = findProduct.get();
 			productDao.delete(product);
-			return new ProductResponse("���歇����");
+			return new ProductResponse("商品已成功刪除");
 		} catch (Exception e) {
 			e.printStackTrace();
-			return new ProductResponse("�����仃���");
+			return new ProductResponse("刪除商品失敗");
 		}
 
 	}
-    //鞈潛頠������������
+
+	// 購物車針對商品做日期範圍搜尋
 	@Override
 	public ProductResponse searchPeriod(ProductRequest productReq) {
 		String firstPosition = productReq.getFirstDay();
 		String secondPosition = productReq.getEndDay();
-		List<String> periodList = new ArrayList<>();// ����風�����
-		
-		
-		
-		 //�LocalDate ��撠�迤蝣箇����
-		 String startDateStr = firstPosition;
-	        String endDateStr = secondPosition;
-	        
-	        LocalDate startDate = LocalDate.parse(startDateStr);
-	        LocalDate endDate = LocalDate.parse(endDateStr);
-	        LocalDate today = LocalDate.now(); //隞予����
+		List<String> periodList = new ArrayList<>();// 收集經歷的日期
 
-	        int firstPositionInt = Integer.parseInt(firstPosition.replaceAll("-", ""));// 20230520
-			int secondPositionInt = Integer.parseInt(secondPosition.replaceAll("-", ""));// 20230522
-	        
-	        if(secondPositionInt<firstPositionInt) {
-	        	return new ProductResponse("�隤�!銝銵迨隢����������");
-	        }
-	        
-	        if (endDate.isAfter(today) ) {
-	            // endDate ��� startDate 頞��捂���嚗��摨������撘虜��內��蝑��
-	        	return new ProductResponse("�隤�!銝銵迨隢��靘��銝���");
-	        }
-	        
-	        if (startDate.isBefore(today.minusDays(30))) {
-	            // endDate ��� startDate 頞��捂���嚗��摨������撘虜��內��蝑��
-	        	return new ProductResponse("�隤�!銝銵迨隢�韏瑕��潸��������30憭奇銝���");
-	        }
+		// 用LocalDate 直接對應正確的時間
+		String startDateStr = firstPosition;
+		String endDateStr = secondPosition;
 
+		LocalDate startDate = LocalDate.parse(startDateStr);
+		LocalDate endDate = LocalDate.parse(endDateStr);
+		LocalDate today = LocalDate.now(); // 今天時間
 
+		int firstPositionInt = Integer.parseInt(firstPosition.replaceAll("-", ""));// 20230520
+		int secondPositionInt = Integer.parseInt(secondPosition.replaceAll("-", ""));// 20230522
 
+		if (secondPositionInt < firstPositionInt) {
+			return new ProductResponse("錯誤!不執行此請求_時間順序不合理");
+		}
 
+		if (endDate.isAfter(today)) {
+			// endDate 或 startDate 超过允许的范围，进行相应处理（例如抛出异常、提示用户等）
+			return new ProductResponse("錯誤!不執行此請求_未來時間_不合理");
+		}
 
+		if (startDate.isBefore(today.minusDays(30))) {
+			// endDate 或 startDate 超过允许的范围，进行相应处理（例如抛出异常、提示用户等）
+			return new ProductResponse("錯誤!不執行此請求_起始值超越今日回朔的30天_不合理");
+		}
 
+		while (!startDate.isAfter(endDate)) {
+			periodList.add(startDate.toString());
+			startDate = startDate.plusDays(1);
 
-	        
-	        
-//	        if (endDate.isAfter(LocalDate.now())) {
-//	            // endDate �隞予銋����摨������撘虜��內��蝑��
-//	        	return new ProductResponse("�隤�!銝銵迨隢��靘������");
-//	        }
-//	        
-//	        if (!(startDate.isBefore(endDate) && startDate.plusDays(30).isBefore(endDate))) {
-//	            return new ProductResponse("�隤�!銝銵迨隢��閰Ｙ�悅����30憭拐����");
-//	        }//��璉��startDate���endDate銋���tartDate����30憭抬�����蝏���endDate銋��
-
-	        while (!startDate.isAfter(endDate)) {
-	            periodList.add(startDate.toString());
-	            startDate = startDate.plusDays(1);
-	            
-	            
-	        }
-		 
-	        
-	       
-		
-
-		
-//		int period = secondPositionInt - firstPositionInt;
-//		int greatMonthPeriod = (31 - firstPositionInt) + secondPositionInt; // 憭扳����
-//		int smallMonthPeriod = (30 - firstPositionInt) + secondPositionInt; // 憭扳����
-//		
-//		
-//		int dayPassThrough = 0;// 閮�����敞��閮�摮�鈭�
-//		String periodStr;// 頧��ayPassThrough
-//		String wholeDay;// 摰���憭�
-
-	
-
-//		// �������瘜�
-//		if (firstPosition.substring(5, 7).equals(secondPosition.substring(5, 7))
-//				&& secondPositionInt > firstPositionInt) {
-//			for (int i = 0; i <= period; i++) {
-//				dayPassThrough = firstPositionInt + i;
-//				periodStr = String.valueOf(dayPassThrough);
-//				String yearPeriod = periodStr.substring(0, 4);// 20230522 ��僑 2023
-//				String monPeriod = periodStr.substring(4, 6);// 20230522 ���� 05
-//				String dayPeriod = periodStr.substring(6);// 20230522 �� 22
-//				wholeDay = yearPeriod + "-" + monPeriod + "-" + dayPeriod;
-//				periodList.add(wholeDay);// ["2023-05-23", "2023-05-24"]
-//			}
-//		}
-//
-//		// 頝冽������瘜�
-//		// 撠��
-//		if (!firstPosition.substring(5, 7).equals(secondPosition.substring(5, 7))
-//			    && secondPositionInt > firstPositionInt) {
-//			    for (int i = 0; i <= smallMonthPeriod; i++) {
-//			        if (( firstPosition.substring(5, 7).equals("04")
-//			            || firstPosition.substring(5, 7).equals("06") || firstPosition.substring(5, 7).equals("09")
-//			            || firstPosition.substring(5, 7).equals("11"))) {
-//
-//			            if (firstPosition.substring(8).equals("30") || // �閬絲憪30憭�
-//			                (String.valueOf(dayPassThrough).indexOf("3")==6 && String.valueOf(dayPassThrough).indexOf("0")==7)) { // ������30憭�
-//			                dayPassThrough = firstPositionInt + 100; // �憓����
-//			                dayPassThrough = dayPassThrough - 30; // 20230530 -> 20230600
-//			            }
-//
-//			            dayPassThrough = firstPositionInt + i;// 20230600->20230601
-//			            periodStr = String.valueOf(dayPassThrough);
-//			            String yearPeriod = periodStr.substring(0, 4);// 20230601 ��僑 2023
-//			            String monPeriod = periodStr.substring(4, 6);// 20230601 ���� 06
-//			            String dayPeriod = periodStr.substring(6);// 20230601 �� 01
-//			            wholeDay = yearPeriod + "-" + monPeriod + "-" + dayPeriod;
-//			            periodList.add(wholeDay);// ["2023-05-31", "2023-06-01"]
-//			        }
-//			    }
-//			}
-//		//====================================================================================================
-//		// 2���
-//		 
-//
-//		
-//		
-//		//=======================================================
-//		//憭扳��
-//		if (!firstPosition.substring(5, 7).equals(secondPosition.substring(5, 7))
-//			    && secondPositionInt > firstPositionInt) {
-//			    for (int i = 0; i <= greatMonthPeriod; i++) {
-//			        if ((firstPosition.substring(5, 7).equals("01") || firstPosition.substring(5, 7).equals("03")
-//			            || firstPosition.substring(5, 7).equals("05") || firstPosition.substring(5, 7).equals("07")
-//			            || firstPosition.substring(5, 7).equals("08") || firstPosition.substring(5, 7).equals("10")
-//			            || firstPosition.substring(5, 7).equals("12"))) {
-//
-//			            if (firstPosition.substring(8).equals("31") || // �閬絲憪31憭�
-//			               (String.valueOf(dayPassThrough).indexOf("3")==6 && String.valueOf(dayPassThrough).indexOf("1")==7)) { // ������31憭�
-//			                dayPassThrough = firstPositionInt + 100; // �憓����
-//			                dayPassThrough = dayPassThrough - 31; // 20230531 -> 20230600
-//
-//			            }
-//
-//			            dayPassThrough = firstPositionInt + i;// 20230600->20230601
-//			            periodStr = String.valueOf(dayPassThrough);
-//			            String yearPeriod = periodStr.substring(0, 4);// 20230601 ��僑 2023
-//			            String monPeriod = periodStr.substring(4, 6);// 20230601 ���� 06
-//			            String dayPeriod = periodStr.substring(6);// 20230601 �� 01
-//			            wholeDay = yearPeriod + "-" + monPeriod + "-" + dayPeriod;
-//			            periodList.add(wholeDay);// ["2023-05-31", "2023-06-01"]
-//			        }
-//			    }
-//			}
-	        
-	    List<Product> productDateList = new ArrayList<>(); // 撌脩��靘����
+		}
+		List<Product> productDateList = new ArrayList<>(); // 已經跑出來的結果
 		for (String item : periodList) {
 			List<Product> dateStartList = productDao.findByDate(item);
 			for (Product resultItem : dateStartList) {
-				Product preInfo= new Product();//�蝑���
+				Product preInfo = new Product();// 單筆資料
 				preInfo.setHsCode(resultItem.getHsCode());
 				preInfo.setName(resultItem.getName());
 				preInfo.setDate(resultItem.getDate());
@@ -439,17 +314,17 @@ public class ProductServiceImpl implements ProductService {
 				preInfo.setPrice(resultItem.getPrice());
 				preInfo.setSellerAccount(resultItem.getSellerAccount());
 				preInfo.setType(resultItem.getType());
-				
-				productDateList.add(preInfo); //�憓憭��”
+
+				productDateList.add(preInfo); // 新增到外面的表
 
 			}
 		}
-		if(productDateList.isEmpty()) {
-			
-			return new ProductResponse("�甇斤�����");
+		if (productDateList.isEmpty()) {
+
+			return new ProductResponse("無此範圍資料");
 		}
 
-		return new ProductResponse(productDateList,"蝭����");
+		return new ProductResponse(productDateList, "範圍日期");
 
 	}
 
